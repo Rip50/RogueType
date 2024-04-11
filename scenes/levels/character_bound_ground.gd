b82fb4dc93638f3faf764a3c1c4808d700a08add
Igor Rodionov <rodionov0504@gmail.com>
2024-04-11 19:16:08 +0200

    Workaround for WorldBoundary2D finite collision bug

---
+++ scenes/levels/character_bound_ground.gd
@@ -0,0 +1,12 @@
+extends StaticBody2D
+
+@export var anchor_character: CharacterBody2D
+
+
+# Workaround for issue https://github.com/godotengine/godot/issues/76917
+func _physics_process(delta: float) -> void:
+	if anchor_character == null:
+		push_warning("anchor_character is null")
+		return
+		
+	self.position.x = anchor_character.position.x

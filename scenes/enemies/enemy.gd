0f7a38a2449463a9f4dab320f97ee3fc0301c92d
Igor Rodionov <rodionov0504@gmail.com>
2024-04-30 19:45:48 +0200

    Enemies are affected by gravity and collide with the floor.
    Enemy spawner's logic simplification

--- scenes/enemies/enemy.gd
+++ scenes/enemies/enemy.gd
@@ -1,7 +1,14 @@
 class_name Enemy
 extends CharacterBody2D
 
-func _physics_process(delta: float) -> void:
+var GRAVITY = 500
+
+func _physics_process(_delta: float) -> void:
 	if velocity == Vector2.ZERO:
 		return
+	if !is_on_floor():
+		velocity.y = GRAVITY
+	else:
+		velocity.y = 0.0
+	
 	move_and_slide()
--- scenes/enemy_spawner.gd
+++ scenes/enemy_spawner.gd
@@ -3,21 +3,16 @@ extends Node
 
 @export var player: Node2D
 @export var enemies: Array[PackedScene]
-@export var ground: Node2D
 @export var vieport_offset := Vector2(0.0, -1.0)
 @export var maximum_enemies_in_group := 1
 
 @onready var timer: Timer = $Timer
 
-const SPRITE_HEIGHT := 102 #px
-
-var ground_level := 0.0
 var viewport: Viewport
+
 
 func _ready() -> void:
 	timer.timeout.connect(_spawn)
-	var shape = ground.get_node("CollisionShape2D").shape
-	ground_level = shape.distance * shape.normal.y + vieport_offset.y
 	viewport = get_viewport()
 	_start_timer_random_timeout()
 
@@ -32,26 +27,26 @@ func _spawn() -> void:
 	
 	_start_timer_random_timeout()
 
+
 func _spawn_enemies(enemy_scene: PackedScene, position: Vector2, count: int) -> void:
 	for i in range(count):
 		var enemy_position = position + Vector2(i * 30.0 + randf_range(5.0, 20.0), 0.0)
 		_spawn_enemy(enemy_scene, enemy_position)
 
+
 func _spawn_enemy(enemy_scene: PackedScene, position: Vector2) -> void:
-		var enemy = enemy_scene.instantiate()
-		get_parent().add_child(enemy)
-		enemy.position = position
+	var enemy = enemy_scene.instantiate()
+	get_parent().add_child(enemy)
+	enemy.position = position
 
 
 func _get_enemy_position(enemy: PackedScene) -> Vector2:
 	var rect = viewport.get_visible_rect()
 	var position_x = player.position.x + rect.size.x + vieport_offset.x
-	
-	#TODO: replace with dinamic sprite height calculation
-	var position_y = ground_level - SPRITE_HEIGHT/2
+	var position_y = player.position.y + vieport_offset.y
 	
 	return Vector2(position_x, position_y)
-	
-	
+
+
 func _start_timer_random_timeout() -> void:
 	timer.start(randf_range(2.0, 10.0))

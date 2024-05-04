5181999a7d31e54368f5c41d7c37618a97807d27
Igor Rodionov <rodionov0504@gmail.com>
2024-05-04 12:06:43 +0200

    Gold counter. Icons for user interface

--- scenes/ui/user_interface.gd
+++ scenes/ui/user_interface.gd
@@ -2,6 +2,7 @@ extends CanvasLayer
 
 @onready var health_bar = $VBoxContainer/HealthbarContainer/HealthBar
 @onready var defense_bar = $VBoxContainer/DefenseBarContainer/DefenseBar
+@onready var gold_counter: Label = $VBoxContainer/GoldContainer/GoldCounter
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,6 +10,7 @@ func _ready() -> void:
 	SignalBus.player_max_health_changed.connect(_update_healthbar_max_value)
 	SignalBus.player_defense_changed.connect(_update_defensebar_value)
 	SignalBus.player_max_defense_changed.connect(_update_defensebar_max_value)
+	SignalBus.player_gold_changed.connect(_update_gold_counter)
 
 
 func _update_healthbar_value(value: int) -> void:
@@ -25,3 +27,7 @@ func _update_defensebar_value(value: int) -> void:
 
 func _update_defensebar_max_value(value: int) -> void:
 	defense_bar.max_value = value
+
+
+func _update_gold_counter(value: int) -> void:
+	gold_counter.text = str(value)
